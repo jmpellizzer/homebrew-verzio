301fbf64645ac9a6f59d1b76d8a0201328a629db
jmpellizzer <joao@zededa.com>
2026-08-19 21:57:38 +0200

    Fix Verzio 1.1.25 architecture selector

--- Casks/verzio.rb
+++ Casks/verzio.rb
@@ -2,7 +2,7 @@
 # frozen_string_literal: true
 
 cask "verzio" do
-  arch arm: "arm64", intel: "37339989d4a956d3c6e720a613caa4fd3bc734c5e44f8933a4ed5fc4877886c1"
+  arch arm: "arm64", intel: "x86_64"
 
   version "1.1.25"
   sha256 arm:   "710a41a7fc04d6432606d8bf938488592e8ed10853beb6b44ba962484e47904b",
